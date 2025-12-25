57a33a3938140d16f3dafe2f0cc40c1f68592375
Aman <81345472+amanr1405@users.noreply.github.com>
2025-12-25 14:02:11 +0530

    completed

--- src/sections/Work.jsx
+++ src/sections/Work.jsx
@@ -5,10 +5,13 @@ function Work() {
     <div className='working' id='Work'>
       <div className='work-left'>
         <h1>System Architecture</h1>
+        <br />
         <img src="architecture.png"/>
       </div>
+      <br />
       <div className='work-right'>
         <h1>Circuit Diagram</h1>
+        <br />
         <img src="circuit.png"/>
       </div>
 
